b4e37e6823a70fdfb146068479c954b5d09fe103
rmusukudabbidi <rmusukudabbidi@slu.edu>
2025-06-23 22:18:50 -0500

    Final Push

--- js/home.js
+++ js/home.js
@@ -11,3 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
   
+
+  //SUbnav in donations
+ function showTab(tab) {
+  const tabs = ['donors', 'become'];
+
+  tabs.forEach(t => {
+    document.getElementById(`tab-${t}`).classList.remove('border-blue-600', 'text-blue-700', 'font-semibold');
+    document.getElementById(`content-${t}`).classList.add('hidden');
+  });
+
+  document.getElementById(`tab-${tab}`).classList.add('border-blue-600', 'text-blue-700', 'font-semibold');
+  document.getElementById(`content-${tab}`).classList.remove('hidden');
+}
+
+// Show the first tab by default
+document.addEventListener('DOMContentLoaded', () => showTab('donors'));
+ 
